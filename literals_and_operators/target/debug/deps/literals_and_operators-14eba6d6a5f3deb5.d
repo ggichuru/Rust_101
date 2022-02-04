7ae43ebfa7a56d26f41a3579e0a769ef2c5a1857
ggichuru <ggichuru.dev@gmail.com>
2022-02-04 14:36:04 +0300

    add gitignore [global]

---
+++ literals_and_operators/target/debug/deps/literals_and_operators-14eba6d6a5f3deb5.d
@@ -0,0 +1,5 @@
+/home/ggichuru/Projects/NasatsLabs/Rust/RustByExample/literals_and_operators/target/debug/deps/literals_and_operators-14eba6d6a5f3deb5.rmeta: src/main.rs
+
+/home/ggichuru/Projects/NasatsLabs/Rust/RustByExample/literals_and_operators/target/debug/deps/literals_and_operators-14eba6d6a5f3deb5.d: src/main.rs
+
+src/main.rs:
---
+++ literals_and_operators/target/debug/deps/literals_and_operators-f1bce00b94f11dc2.d
@@ -0,0 +1,5 @@
+/home/ggichuru/Projects/NasatsLabs/Rust/RustByExample/literals_and_operators/target/debug/deps/literals_and_operators-f1bce00b94f11dc2.rmeta: src/main.rs
+
+/home/ggichuru/Projects/NasatsLabs/Rust/RustByExample/literals_and_operators/target/debug/deps/literals_and_operators-f1bce00b94f11dc2.d: src/main.rs
+
+src/main.rs:
