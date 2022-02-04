10105179aaa21b3c0351d1a73f77dcabb81aa9f1
ggichuru <ggichuru.dev@gmail.com>
2022-02-04 15:56:20 +0300

    i

--- primitives/target/debug/deps/primitives-9369c6e8ae529782.d
+++
@@ -1,5 +0,0 @@
-/home/ggichuru/Projects/NasatsLabs/Rust/RustByExample/primitives/target/debug/deps/primitives-9369c6e8ae529782: src/main.rs
-
-/home/ggichuru/Projects/NasatsLabs/Rust/RustByExample/primitives/target/debug/deps/primitives-9369c6e8ae529782.d: src/main.rs
-
-src/main.rs:
--- primitives/target/debug/deps/primitives-f2bd704e41c98247.d
+++
@@ -1,5 +0,0 @@
-/home/ggichuru/Projects/NasatsLabs/Rust/RustByExample/primitives/target/debug/deps/primitives-f2bd704e41c98247.rmeta: src/main.rs
-
-/home/ggichuru/Projects/NasatsLabs/Rust/RustByExample/primitives/target/debug/deps/primitives-f2bd704e41c98247.d: src/main.rs
-
-src/main.rs:
