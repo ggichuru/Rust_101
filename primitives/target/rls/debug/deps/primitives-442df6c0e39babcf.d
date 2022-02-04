c469fd35eb8101f3a41a20aad67a32a0a9c7def0
ggichuru <ggichuru.dev@gmail.com>
2022-02-04 14:05:28 +0300

    init: Primitives

---
+++ primitives/target/rls/debug/deps/primitives-442df6c0e39babcf.d
@@ -0,0 +1,5 @@
+/home/ggichuru/Projects/NasatsLabs/Rust/RustByExample/primitives/target/rls/debug/deps/primitives-442df6c0e39babcf.rmeta: src/main.rs
+
+/home/ggichuru/Projects/NasatsLabs/Rust/RustByExample/primitives/target/rls/debug/deps/primitives-442df6c0e39babcf.d: src/main.rs
+
+src/main.rs:
---
+++ primitives/target/rls/debug/deps/primitives-f2bd704e41c98247.d
@@ -0,0 +1,5 @@
+/home/ggichuru/Projects/NasatsLabs/Rust/RustByExample/primitives/target/rls/debug/deps/primitives-f2bd704e41c98247.rmeta: src/main.rs
+
+/home/ggichuru/Projects/NasatsLabs/Rust/RustByExample/primitives/target/rls/debug/deps/primitives-f2bd704e41c98247.d: src/main.rs
+
+src/main.rs:
